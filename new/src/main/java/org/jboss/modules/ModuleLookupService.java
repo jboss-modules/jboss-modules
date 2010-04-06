b54158edf472f0ad306cad06d63f2d9c37642efa
David M. Lloyd <dmlloyd@flurg.com>
2010-04-06 11:18:37 -0500

    Simple module lookup service

---
+++ new/src/main/java/org/jboss/modules/ModuleLookupService.java
@@ -0,0 +1,55 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.modules;
+
+/**
+ * A simple POJO which can look up a module by name.
+ *
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
+ */
+public final class ModuleLookupService {
+    private static final ModuleLookupService INSTANCE = new ModuleLookupService();
+
+    private ModuleLookupService() {
+    }
+
+    /**
+     * Get a module with the given identifier.
+     *
+     * @param identifier the identifier
+     * @return the module
+     * @throws ModuleLoadException if the module is not found
+     */
+    public Module get(String identifier) throws ModuleLoadException {
+        return Module.getModule(ModuleIdentifier.fromString(identifier));
+    }
+
+    /**
+     * Get the instance.
+     *
+     * @return the instance
+     */
+    public static ModuleLookupService getInstance() {
+        return INSTANCE;
+    }
+}
