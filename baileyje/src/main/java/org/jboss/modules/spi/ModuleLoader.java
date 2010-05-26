8219b22d52b58f628518046ba0156b3d0857ab48
John Bailey <baileyje@gmail.com>
2010-05-26 10:26:21 -0500

    remove cruft

--- baileyje/src/main/java/org/jboss/modules/spi/ModuleLoader.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.modules.spi;
-
-/**
- * @author <a href="mailto:jbailey@redhat.com">John Bailey</a>
- */
-public interface ModuleLoader
-{
-   Module loadModule(ModuleId moduleId);
-}
