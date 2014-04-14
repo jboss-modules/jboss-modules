2c99e3b9b2638bb3faa74f43755f9c385c79e295
Jan Kalina <xkalin03@stud.fit.vutbr.cz>
2014-04-14 12:35:07 +0200

    Added flag for allowing use policy files with posibility of refresh This patch add property jboss.modules.policy-refreshable - if it is true, non-static permission will be used in ProtectionDomain.
    For use, add following into your start script of WildFly:
    JAVA_OPTS="$JAVA_OPTS -Djboss.modules.policy-refreshable=true"

--- src/main/java/org/jboss/modules/ModuleClassLoader.java
+++ src/main/java/org/jboss/modules/ModuleClassLoader.java
@@ -63,6 +63,8 @@ import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
  */
 public class ModuleClassLoader extends ConcurrentClassLoader {
 
+    private static final boolean POLICY_REFRESHABLE;
+
     private static final boolean POLICY_PERMISSIONS;
 
     static final AtomicBoolean POLICY_READY = new AtomicBoolean();
@@ -76,6 +78,7 @@ public class ModuleClassLoader extends ConcurrentClassLoader {
         if (! parallelOk) {
             throw new Error("Failed to register " + ModuleClassLoader.class.getName() + " as parallel-capable");
         }
+        POLICY_REFRESHABLE = Boolean.parseBoolean(System.getProperty("jboss.modules.policy-refreshable", "false"));
         POLICY_PERMISSIONS = Boolean.parseBoolean(System.getProperty("jboss.modules.policy-permissions", "false"));
     }
 
@@ -385,7 +388,9 @@ public class ModuleClassLoader extends ConcurrentClassLoader {
             ProtectionDomain protectionDomain = map.get(codeSource);
             if (protectionDomain == null) {
                 final PermissionCollection permissions = module.getPermissionCollection();
-                if (POLICY_PERMISSIONS && POLICY_READY.get()) {
+                if (POLICY_REFRESHABLE) {
+                    protectionDomain = new ProtectionDomain(codeSource, permissions, this, null); // staticPermission=false
+                }else if (POLICY_PERMISSIONS && POLICY_READY.get()) {
                     final Policy policy = AccessController.doPrivileged(GET_POLICY_ACTION);
                     if (policy != null) {
                         final PermissionCollection policyPermissions = policy.getPermissions(codeSource);
