ac0443dc065bb5f69e86411b0c89680a219b4be0
David M. Lloyd <david.lloyd@redhat.com>
2014-08-05 10:42:19 -0500

    Restore 1.6 compatibility

--- src/main/java/org/jboss/modules/JarFileResourceLoader.java
+++ src/main/java/org/jboss/modules/JarFileResourceLoader.java
@@ -72,7 +72,7 @@ final class JarFileResourceLoader extends AbstractResourceLoader implements Iter
     private final File fileOfJar;
 
     // protected by {@code this}
-    private final Map<CodeSigners, CodeSource> codeSources = new HashMap<>();
+    private final Map<CodeSigners, CodeSource> codeSources = new HashMap<CodeSigners, CodeSource>();
 
     JarFileResourceLoader(final String rootName, final JarFile jarFile) {
         this(rootName, jarFile, null);
