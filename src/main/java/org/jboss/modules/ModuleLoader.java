31665a814f2723fd8734c98b823a498ce3419397
Richard Opalka <opalka.richard@gmail.com>
2016-02-11 15:51:31 +0100

    [MODULES-241] fixing alias modules leaks

--- src/main/java/org/jboss/modules/ModuleLoader.java
+++ src/main/java/org/jboss/modules/ModuleLoader.java
@@ -61,6 +61,7 @@ import javax.management.ObjectName;
  *
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  * @author <a href="mailto:jbailey@redhat.com">John Bailey</a>
+ * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
  * @author Jason T. Greene
  *
  * @apiviz.landmark
@@ -254,7 +255,12 @@ public abstract class ModuleLoader {
     protected final Module loadModuleLocal(ModuleIdentifier identifier) throws ModuleLoadException {
         FutureModule futureModule = moduleMap.get(identifier);
         if (futureModule != null) {
-            return futureModule.getModule();
+            Module fModule = futureModule.getModule();
+            if (fModule.isRemoved()) {
+                moduleMap.remove(identifier, futureModule); // clean up obsolete alias modules
+            } else {
+                return futureModule.getModule();
+            }
         }
 
         FutureModule newFuture = new FutureModule(identifier);
@@ -344,6 +350,8 @@ public abstract class ModuleLoader {
         final ModuleIdentifier id = module.getIdentifier();
         final FutureModule futureModule = moduleMap.get(id);
         if (futureModule != null && futureModule.module == module) {
+            final Module fModule = (Module) futureModule.module;
+            fModule.remove();
             moduleMap.remove(id, futureModule);
         }
     }
