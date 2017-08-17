5b1b76b490173b5038e833c4b798266f967cbed9
Bob McWhirter <bob@mcwhirter.org>
2017-08-17 11:07:35 -0400

    MODULES-303 - Interpolate ${props} in settings.xml
    Since settings.xml can use ${props} and ${env.ENV} within
    the <localRepository> tag, we should interpolate them if they
    are present.

--- src/test/java/org/jboss/modules/maven/MavenSettingsTest.java
+++ src/test/java/org/jboss/modules/maven/MavenSettingsTest.java
@@ -73,6 +73,23 @@ public class MavenSettingsTest {
 
     }
 
+    @Test
+    public void testInterpolatedLocalRepo() throws Exception {
+        Path userRepo = tmpdir.newFolder(".m2", "repository").toPath();
+        String userHome = System.getProperty("user.home");
+        System.setProperty("user.home", tmpdir.getRoot().getAbsolutePath());
+
+        try {
+            clearCachedSettings();
+            MavenSettings settings = new MavenSettings();
+
+            MavenSettings.parseSettingsXml(Paths.get(MavenSettingsTest.class.getResource("settings-interpolated-local-repo.xml").toURI()), settings);
+            Assert.assertEquals(Paths.get(tmpdir.getRoot().getAbsolutePath() + "/.mvnrepository"), settings.getLocalRepository());
+        } finally {
+            System.setProperty("user.home", userHome);
+        }
+    }
+
 
     @Test
     public void testProxies() throws Exception {
@@ -130,5 +147,46 @@ public class MavenSettingsTest {
         ArtifactCoordinates coordinates = ArtifactCoordinates.fromString("org.wildfly.core:wildfly-version:2.0.5.Final-20151222.144931-1");
         String path = coordinates.relativeArtifactPath('/');
         Assert.assertEquals("org/wildfly/core/wildfly-version/2.0.5.Final-SNAPSHOT/wildfly-version-2.0.5.Final-20151222.144931-1", path);
+    }
+
+    @Test
+    public void testInterpolateVariablesOneVariable() throws Exception {
+        try {
+            System.setProperty( "test.user.home", "/home/bob" );
+            Assert.assertEquals("/home/bob/.m2/repository", MavenSettings.interpolateVariables("${test.user.home}/.m2/repository"));
+        } finally {
+            System.clearProperty("test.user.home");
+        }
+    }
+
+    @Test
+    public void testInterpolateVariablesTwoVariables() throws Exception {
+        try {
+            System.setProperty( "test.user.home", "/home/bob" );
+            System.setProperty( "test.repo.dir", "repository" );
+            Assert.assertEquals("/home/bob/.m2/repository", MavenSettings.interpolateVariables("${test.user.home}/.m2/${test.repo.dir}"));
+        } finally {
+            System.clearProperty("test.user.home");
+        }
+    }
+
+    @Test
+    public void testInterpolateVariablesInvalidExpression() throws Exception {
+        try {
+            System.setProperty( "test.user.home", "/home/bob" );
+            Assert.assertEquals("${test.user.home/.m2/repository", MavenSettings.interpolateVariables("${test.user.home/.m2/repository"));
+        } finally {
+            System.clearProperty("test.user.home");
+        }
+    }
+
+    @Test
+    public void testInterpolateVariablesInvalidExpression2() throws Exception {
+        try {
+            System.setProperty( "test.user.home", "/home/bob" );
+            Assert.assertEquals("/home/bob/.m2/${repoName", MavenSettings.interpolateVariables("${test.user.home}/.m2/${repoName"));
+        } finally {
+            System.clearProperty("test.user.home");
+        }
     }
 }
