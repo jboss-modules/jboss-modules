499aef87a0a010da3a3d7c9d430ed41c3eac7255
Richard Opálka <opalka.richard@gmail.com>
2022-01-26 21:50:27 +0100

    Updates for 2.0 plus update index

---
+++ 1.10.0.Final/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"__redirected"},{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.jboss.modules"},{"l":"org.jboss.modules.filter"},{"l":"org.jboss.modules.log"},{"l":"org.jboss.modules.management"},{"l":"org.jboss.modules.maven"},{"l":"org.jboss.modules.ref"},{"l":"org.jboss.modules.security"},{"l":"org.jboss.modules.xml"}]
